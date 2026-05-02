014111d4f3f8dd50262fee43f5ec6e2e32a93be7
Khushi Mogha <khushimogha912@gmail.com>
2026-05-03 00:14:48 +0530

    Day3

---
+++ slidingwindow.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class slidingwindow {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int n= sc.nextInt();
+        int[] arr = {2,4,1,7,1,8,5};
+        int n=arr.length;
+        int maxsum=0;
+        int k=3;
+        for(int i=0;i<k;i++){
+            maxsum+=arr[i];
+        }
+        int sum=maxsum;
+        for(int i=0;i+k<=n-1;i++){        
+            sum=sum-arr[i]+arr[i+k];
+            maxsum=Math.max(maxsum,sum);
+        }
+        System.out.println(maxsum);
+    }
+    
+}
