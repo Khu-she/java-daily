d893fabbe138abab23a47e4cc67b2bd4896d5587
Khushi Mogha <khushimogha912@gmail.com>
2026-05-05 10:51:59 +0530

    Day6

---
+++ ElementbySign.java
@@ -0,0 +1,31 @@
+public class ElementbySign {
+    static void sign(int[] arr){
+        int n= arr.length;
+        int[] pos=new int[n];
+        int[] neg=new int[n];
+        int[] result=new int[n];
+        int ne=0;
+      int po=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                pos[po++]=arr[i];
+            }
+            else{
+                neg[ne++]=arr[i];
+            }
+        }
+        for(int i=0;i<n/2;i++){
+            result[2*i]=pos[i];
+            result[2*i+1]=neg[i];
+        }
+    
+        
+        for(int i:result){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={3,1,-2,-5,2,-4};
+        sign(arr);
+    }
+}
